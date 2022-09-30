88de281e0706615c6c22c76a8c1b81cb97d74c4d
b-illy <19840447+b-illy@users.noreply.github.com>
2022-09-30 21:46:16 +0100

    add proper error handling to command handler

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -9,12 +9,20 @@ export default async (client, i) => {
                 i.createMessage("Command no longer exists");
                 return;
             }
+            // acknowledge command, giving the "waiting..." text response to show that it is working
             await i.acknowledge();
-            const res = await client.commands.get(i.data.name).run(client, i, null, i.member, i.channel)
-            i.createFollowup(
-                res.content,
-                res.file ?? null
-            );
+            try {
+                // run the command and store the outputted message
+                const res = await client.commands.get(i.data.name).run(client, i, null, i.member, i.channel);
+                // send the follow up message
+                i.createFollowup(
+                    res.content,
+                    res.file ?? null
+                );
+            } catch (err) {
+                i.createFollowup("An error occured while running this command.");
+                console.error(err);
+            }
             break;
         case 3:  // message component
             console.log("WARNING: message component update received, but this is not yet implemented");
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,23 +8,36 @@ export default async (client, message) => {
         message.args = args;
 
         if (client.commands.get(command)) {
-            // if content is string, convert to object
-            let res = await client.commands.get(command).run(client, null, message, message.member, message.channel);
-            if (typeof res.content == "string") {
-                res.content = {
-                    content: res.content
+            try {
+                // run the command and store the outputted message
+                let res = await client.commands.get(command).run(client, null, message, message.member, message.channel);
+                // if content is string, convert to object
+                if (typeof res.content == "string") {
+                    res.content = {
+                        content: res.content
+                    }
                 }
+                // add content.messageReference to make this message a reply
+                res.content.messageReference = {
+                    messageID: message.id,
+                    failIfNotExists: false
+                };
+    
+                // send response message
+                message.channel.createMessage(
+                    res.content,
+                    res.file ?? null
+                );
+            } catch (err) {
+                message.channel.createMessage({
+                    content: "An error occured while running this command.",
+                    messageReference: {
+                        messageID: message.id,
+                        failIfNotExists: false
+                    }
+                });
+                console.error(err);
             }
-            // add content.messageReference to make this message a reply
-            res.content.messageReference = {
-                messageID: message.id,
-                failIfNotExists: false
-            };
-
-            message.channel.createMessage(
-                res.content,
-                res.file ?? null
-            );
         } else {
             return;
         }
